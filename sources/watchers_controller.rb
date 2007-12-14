36211941d80ea7fd119e3b805a8af1b6a1034473
jsmith <jsmith@foo.bar>
2007-12-14 10:25:20 +0100

    Modified one file
    Another line of message

--- sources/watchers_controller.rb
+++ sources/watchers_controller.rb
@@ -19,15 +19,6 @@ class WatchersController < ApplicationController
   layout 'base'
   before_filter :require_login, :find_project, :check_project_privacy
   
-  def add
-    user = User.current
-    @watched.add_watcher(user)
-    respond_to do |format|
-      format.html { render :text => 'Watcher added.', :layout => true }
-      format.js { render(:update) {|page| page.replace_html 'watcher', watcher_link(@watched, user)} }
-    end
-  end
-  
   def remove
     user = User.current
     @watched.remove_watcher(user)
@@ -40,7 +31,7 @@ class WatchersController < ApplicationController
 private
   def find_project
     klass = Object.const_get(params[:object_type].camelcase)
-    return false unless klass.respond_to?('watched_by')
+    return true unless klass.respond_to?('watched_by')
     @watched = klass.find(params[:object_id])
     @project = @watched.project
   rescue
